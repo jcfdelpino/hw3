3f92411188f75cb9cad4f640a1c3caad8cfbbf2b
jcfdelpino <juan.delpino@kellogg.northwestern.edu>
2022-02-17 23:43:56 +0000

    v2

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,6 +2,21 @@ class PlacesController < ApplicationController
 
     def index
         @places = Place.all
+        @name = params["name"]
     end
+
+    def show
+        @places = Place.find(params["id"])
+      end
     
+    def new
+        @places = Place.new
+    end
+
+    def create
+       @place = Place.new(params["name"])
+       @place.save
+       redirect_to "/places"
+    end
+
 end
---
+++ data_pop.rb
@@ -0,0 +1,1 @@
+puts Place.all.inspect
