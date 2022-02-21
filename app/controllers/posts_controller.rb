9ab8c0c886a3ca1b4b3bfc1d0c31b95becb8b49a
jcfdelpino <juan.delpino@kellogg.northwestern.edu>
2022-02-21 00:49:22 +0000

    save

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,12 +6,13 @@ class PostsController < ApplicationController
     
       def new
         @post = Post.new
+        @post.place_id = params["place_id"]
       end
     
       def create
         @post = Post.new(params["post"])
         @post.save
-        redirect_to "/posts"
+        redirect_to "/places/#{@post.place_id}"
       end
       
 end
